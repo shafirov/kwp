4183aadfee0045d222abb0bf7ad6b23800119440
Maxim Shafirov <max@jetbrains.com>
2016-08-08 19:21:01 +0300

    Initial

---
+++ src/main/groovy/KWP.groovy
@@ -0,0 +1,85 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.tasks.bundling.Jar
+
+import java.util.zip.ZipFile
+
+class KWP implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        target.task(dependsOn: Jar, 'webpack_loader') << {
+            def dependencies = new LinkedHashSet<File>()
+            def sources = new LinkedHashSet<String>()
+            collectDependencies(project, sources, dependencies)
+
+            def targetDir = new File(rootProject.buildDir, "kjs")
+            targetDir.mkdirs()
+
+            writeSafely(new File(targetDir, "__modules.js")) { buf ->
+                dependencies.each {jar ->
+                    def m = unzipSafely(jar, targetDir)
+                    buf.append("require('${m.absolutePath}');\n")
+                }
+            }
+
+            writeSafely(new File(targetDir, "__deps.txt")) { buf ->
+                sources.each {
+                    buf.append("$it\n")
+                }
+            }
+        }
+    }
+
+    void collectDependencies(Project project, Set<String> sources, Set<File> dependencies) {
+        def configuration = project.configurations.findByName("default")
+        configuration.allDependencies.each {
+            if (it instanceof ProjectDependency) {
+                collectDependencies(it.dependencyProject, sources, dependencies)
+            }
+        }
+
+        dependencies.addAll(configuration.resolve())
+        dependencies.addAll(configuration.allArtifacts.files)
+
+        sources.add(new File(project.projectDir, "src/main/kotlin").getAbsolutePath())
+    }
+
+    File unzipSafely(File jar, File targetFolder) {
+        def targetFile = new File(targetFolder, removeSuffix(jar.name, ".jar") + ".js")
+        if (!targetFile.exists() || targetFile.lastModified() != jar.lastModified()) {
+            ZipFile zip = new ZipFile(jar)
+            def targets = 0
+            for (zipEntry in zip.entries()) {
+                if (zipEntry.isDirectory()) continue
+
+                if (zipEntry.name.endsWith(".meta.js")) {
+                    new File(targetFolder, zipEntry.name).text = zip.getInputStream(zipEntry).text
+                    targets++
+                }
+                else if (zipEntry.name.endsWith(".js")) {
+                    targetFile.text = zip.getInputStream(zipEntry).text
+                    targets++
+                }
+
+                if (targets == 2) break
+            }
+
+            targetFile.setLastModified(jar.lastModified())
+        }
+
+        return targetFile
+    }
+
+    String removeSuffix(String str, String suffix) {
+        if (str.endsWith(suffix)) return str.substring(0, str.length() - suffix.length())
+        return str
+    }
+
+    void writeSafely(File file, Closure<StringBuilder> builder) {
+        def buffer = new StringBuilder()
+        builder(buffer)
+        def contents = buffer.toString()
+        if (!file.exists() || file.text != contents) file.text = contents
+    }
+}
---
+++ src/main/js/kwp.js
@@ -0,0 +1,64 @@
+var path = require("path");
+var fs = require('fs')
+var exec = require('child_process').exec
+
+var isWin = /^win/.test(process.platform);
+
+module.exports = function(content) {
+    var loader = this
+    loader.cacheable()
+    var callback = loader.async()
+
+    var fail = function(message) {
+        callback(message)
+    }
+
+    var success = function (res, map) {
+        callback(null, res, map)
+    }
+
+    var buildFile = path.resolve(loader.resourcePath);
+    var project = loader.resourceQuery.substr(1)
+
+    var gradle = "./gradlew"
+    if (isWin) {
+        gradle += ".bat"
+    }
+
+    gradle = path.resolve(gradle)
+    if (!gradle) {
+        fail("Cannot find ./gradlew")
+        return
+    }
+
+    if (!buildFile) {
+        fail("Cannot resolve build file: " + buildArg)
+        return
+    }
+
+    console.time("Done Gradle")
+    var cmdline = gradle + " " + "--build-file " + buildFile + " " + project + ":webpack_loader";
+    console.log("Running Gradle: " + cmdline)
+
+    exec(cmdline, function(err, stdout, stderr) {
+        console.log(stderr)
+        if (err) {
+            console.log(stdout)
+            fail(err)
+        }
+        else {
+            // console.log(stdout)
+            var deps = fs.readFileSync(path.resolve('build/kjs/__deps.txt'), "UTF-8").split("\n")
+
+            for (var i = 0; i < deps.length; i++) {
+                var t = deps[i].trim()
+                if (t.length > 0) {
+                    loader.addContextDependency(t)
+                }
+            }
+
+            success(fs.readFileSync(path.resolve('build/kjs/__modules.js'), "UTF-8"))
+            console.timeEnd("Done Gradle")
+        }
+    })
+}
