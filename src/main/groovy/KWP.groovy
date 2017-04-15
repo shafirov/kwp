edebc59f4096e9a8adf03986618698c72e369a37
Maxim Shafirov <max@jetbrains.com>
2017-04-15 13:50:02 +0300

    Simplify the goal of KWP. Now it just provides a kwp task, which places generated javascript and dependencies into build/kwp, which need to be specified as modulesDirectories in webpack.config along with node_modules

--- src/main/groovy/KWP.groovy
+++ src/main/groovy/KWP.groovy
@@ -5,97 +5,51 @@ import org.gradle.api.artifacts.ProjectDependency
 import java.util.zip.ZipFile
 
 class KWP implements Plugin<Project> {
-    private static def loaderText = KWP.class.getResourceAsStream("/kwp.js").text
-
-    void installKwp(File targetDir, boolean force) {
-        def file = new File(targetDir, "kwp.js")
-        if (!file.exists() || force) {
-            writeSafely(file) { buf ->
-                buf.append(loaderText)
-            }
-        }
-    }
 
     @Override
     void apply(Project target) {
         def rootProject = target.rootProject
         def targetDir = new File(rootProject.buildDir, "kwp")
 
-        rootProject.afterEvaluate {
-            installKwp(targetDir, true)
-        }
+        target.task(dependsOn: "jar", "kwp") {
+            doLast {
+                def dependencies = new LinkedHashSet<File>()
+                collectDependencies(project, dependencies)
 
-        rootProject.tasks['clean'] << {
-            installKwp(targetDir, false)
-        }
-
-        target.task(dependsOn: 'jar', 'webpack_loader') << {
-            def dependencies = new LinkedHashSet<File>()
-            def sources = new LinkedHashSet<String>()
-            collectDependencies(project, sources, dependencies)
-
-            writeSafely(new File(targetDir, "__modules.txt")) { buf ->
                 dependencies.each {jar ->
-                    def m = unzipSafely(jar, targetDir)
-                    if (m != null) {
-                        def moduleName = m.name.substring(0, m.name.lastIndexOf('.'))
-                        buf.append("${moduleName}:${normalizedAbsolutePath(m)}\n")
-                    }
-                }
-            }
-
-            writeSafely(new File(targetDir, "__sources.txt")) { buf ->
-                sources.each {
-                    buf.append("$it\n")
+                    unzipSafely(jar, targetDir)
                 }
             }
         }
     }
 
-    void collectDependencies(Project project, Set<String> sources, Set<File> dependencies) {
+    void collectDependencies(Project project, Set<File> dependencies) {
         def configuration = project.configurations.findByName("default")
         configuration.allDependencies.each {
             if (it instanceof ProjectDependency) {
-                collectDependencies(it.dependencyProject, sources, dependencies)
+                collectDependencies(it.dependencyProject, dependencies)
             }
         }
 
         dependencies.addAll(configuration.resolve())
         dependencies.addAll(configuration.allArtifacts.files)
-
-        sources.add(normalizedAbsolutePath(new File(project.projectDir, "src/main/kotlin")))
     }
 
-    File unzipSafely(File jar, File targetFolder) {
+    void unzipSafely(File jar, File targetFolder) {
         def timestampFile = new File(targetFolder, jar.name + ".timestamp")
-        def targetFile = null
         if (!timestampFile.exists() || timestampFile.lastModified() != jar.lastModified()) {
             ZipFile zip = new ZipFile(jar)
-            def targets = 0
             for (zipEntry in zip.entries()) {
                 if (zipEntry.isDirectory()) continue
 
-                if (zipEntry.name.endsWith(".meta.js") || zipEntry.name.endsWith('.js.map')) {
+                if (zipEntry.name.endsWith(".js") || zipEntry.name.endsWith('.js.map')) {
                     writeSafely(new File(targetFolder, zipEntry.name), zip.getInputStream(zipEntry).text)
-                    targets++
                 }
-                else if (zipEntry.name.endsWith(".js")) {
-                    targetFile = new File(targetFolder, zipEntry.name)
-                    writeSafely(targetFile, zip.getInputStream(zipEntry).text)
-                    targets++
-                }
-
-                if (targets == 3) break
             }
 
-            timestampFile.text = targetFile?.absolutePath ?: "notfound.js"
+            writeSafely(timestampFile, jar.lastModified().toString())
             timestampFile.setLastModified(jar.lastModified())
         }
-        else {
-            targetFile = new File(timestampFile.text)
-        }
-
-        return targetFile
     }
 
     String removeSuffix(String str, String suffix) {
@@ -114,9 +68,5 @@ class KWP implements Plugin<Project> {
         def buffer = new StringBuilder()
         builder(buffer)
         writeSafely(file, buffer.toString())
-    }
-
-    String normalizedAbsolutePath(File file) {
-        return file.absolutePath.replace('\\', '/')
     }
 }
